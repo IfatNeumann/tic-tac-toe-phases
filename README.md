eefdb694e69ddc5fe2214885622465170ed556b4
IfatNeumann <ineumann19@gmail.com>
2022-01-29 20:57:27 +0000

    phase 3 - check win or draw + add favicon

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Each commit is a phase in the build of the game, and each phase has demo link.
 [Phase 1](https://tic-tac-toe-phase-1.netlify.app/): In phase 1, clicking the Start Game button will create a 3x3 board, and clicking the cells will mark them with X and O. X's and O's moves aren't saved.
 
 [Phase 2](https://tic-tac-toe-phase-2.netlify.app/): In phase 2, X's and O's moves are saved, but there is no check for winning patterns.
+
+[Phase 3](https://tic-tac-toe-phase-3.netlify.app/): In phase 3, X's and O's moves are checked with the possible winning patterns. In this phase we declare win or draw, but the score isn't saved.
+
+I also added a favicon.
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,17 @@ var selections = new Array();
 //[0,1,2]
 //[3,4,5]
 //[6,7,8]
+var win_patterns =  [
+	[1,1,1,0,0,0,0,0,0], // [0,1,2]
+	[0,0,0,1,1,1,0,0,0], // [3,4,5]
+	[0,0,0,0,0,0,1,1,1], // [6,7,8]
+	[1,0,0,1,0,0,1,0,0], // [0,3,6]
+	[0,1,0,0,1,0,0,1,0], // [1,4,7]
+	[0,0,1,0,0,1,0,0,1], // [2,5,8]
+	[1,0,0,0,1,0,0,0,1], // [0,4,8]
+	[0,0,1,0,1,0,1,0,0], // [2,4,6]
+];
+
 selections['X'] = [0,0,0,0,0,0,0,0,0];
 selections['O'] = [0,0,0,0,0,0,0,0,0];
 
@@ -40,7 +51,8 @@ function markCheck(obj){
 	obj.value = turn;
 	var cell = Number(obj.id);
 	selections[turn][cell] = 1;
-	console.log(`player ${turn} marked ${obj.id}!`);
+	total_turns_played++;
+	checkPlayerHasAnyWinningPattern();
 
 	if (turn == 'X' ) {
 		obj.setAttribute("class", 'green-player');
@@ -51,10 +63,53 @@ function markCheck(obj){
 	}
 
 	obj.setAttribute("disabled", 'disabled');
-	total_turns_played++;
-	if (total_turns_played == 9){
-		console.log(`player X marked [${selections['X']}]`);
-		console.log(`player O marked [${selections['O']}]`);
-		alert("Board is full!");
+}
+
+function checkPlayerHasAnyWinningPattern() {
+	gameOver = false; //game is over if someone wins
+
+	for (var p=0; p < win_patterns.length; p++) {
+		if (gameOver != true) { 
+			gameOver = isContainingThisWinningPattern(selections[turn], win_patterns[p]);
+
+			if ( gameOver === true ) {
+				
+				// On winning disabled all boxes
+				disableAllCells();
+
+				alert('Player '+turn+' Won !!');
+				break;
+			} 
+		}
+	}
+
+	// If no one wins; declare DRAW
+	if (total_turns_played == 9 && gameOver === false){
+		alert('Game Draw!');
+		gameOver = true;	
 	}
 }
+
+// Verifying player's selections with a specific winning pattern
+function isContainingThisWinningPattern(player_selections, win_pattern){
+	var match = 0;
+	for (var i=0; i < 9; i++) {
+		if(player_selections[i] + win_pattern[i] == 2){
+			match ++;
+		}
+	}
+
+	if (match == 3){
+		return true;
+	}
+
+	return false;
+}
+
+function disableAllCells() {
+
+	var elements = document.getElementsByClassName("grid-cell");
+	for (var i = 0; i < elements.length; i++) {
+	  elements[i].disabled =true;
+	}
+} 
